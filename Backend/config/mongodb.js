1ff78258123ff644c2c8414aa06e8ddf3bfa5c00
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-09-04 10:34:37 +0530

    chnage db connection

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,8 +1,23 @@
 import mongoose from "mongoose";
 
+// const connectDB = async () => {
+//   mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+//   await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+// }
+
+
 const connectDB = async () => {
-  mongoose.connection.on('connected', ()=> console.log('Database Connected'))
-  await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
-}
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Database Connected");
+  } catch (error) {
+    console.error("❌ Database Connection Error:", error.message);
+    process.exit(1);
+  }
+};
+
 
 export default connectDB
