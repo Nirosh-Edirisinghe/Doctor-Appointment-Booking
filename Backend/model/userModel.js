da116b7b8527c76e677ec7b9fd6c46387b2058c3
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-10-01 00:49:54 +0530

    chnage uere model feiled name

--- Backend/model/userModel.js
+++ Backend/model/userModel.js
@@ -8,7 +8,7 @@ const  userSchema = new mongoose.Schema({
   address: {type:Object, default:{line1:'',line2:''}},
   gender: {type:String,default:"Not Selected"},
   dob: {type:String,default:"Not Selected"},
-  gender: {type:String,default:"0000000000"},
+  phone: {type:String,default:"0000000000"},
 })
 
 const userModel  = mongoose.models.user || mongoose.model('user',userSchema)
